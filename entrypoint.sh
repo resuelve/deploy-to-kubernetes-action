72836829222a5a7ec186831f42bfdddefb6cecf5
Erick Navarro <ericknavarro@resuelve.mx>
2023-12-12 12:48:54 -0500

    Add support to update external secret (#1)
    Add a new input to define the name of the external secrets associated
    to a deployment so it can be synced before deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -l
+# exit on any error
+set -e
 
 KUBERNETES_CERT="${INPUT_CERT}"
 KUBERNETES_SERVER="${INPUT_SERVER}"
@@ -10,8 +12,9 @@ NAMESPACE="${INPUT_NAMESPACE}"
 DEPLOYMENT="${INPUT_NAME}"
 IMAGE="${INPUT_IMAGE}"
 CONTAINER="${INPUT_CONTAINER}"
+EXTERNAL_SECRET="${INPUT_EXTERNAL_SECRET}"
 
-echo "${KUBERNETES_CERT}" | base64 -d > ca.crt
+echo "${KUBERNETES_CERT}" | base64 -d >ca.crt
 
 kubectl config set-cluster "${CLUSTER_NAME}" --server="${KUBERNETES_SERVER}" --certificate-authority=ca.crt
 
@@ -21,12 +24,32 @@ kubectl config use-context "${CONTEXT}"
 
 kubectl config set-credentials "$CONTEXT" --token="$KUBERNETES_TOKEN"
 
+if [ "$EXTERNAL_SECRET" ]; then
+  if [ ! "$EXTERNAL_SECRET" ]; then
+    echo "External secret name was not defined."
+    exit 1
+  fi
+
+  echo "Checking if $EXTERNAL_SECRET exists..."
+
+  if [ "$(kubectl -n "$NAMESPACE" get es "$EXTERNAL_SECRET" -o json | jq '.status.conditions | first | .reason' | xargs)" == "SecretSynced" ]; then
+    echo "$EXTERNAL_SECRET OK"
+  else
+    echo "$EXTERNAL_SECRET has an unvalid status, please review it"
+    exit 1
+  fi
+
+  echo "Syncing secret before deployment image update..."
+
+  kubectl -n "$NAMESPACE" annotate es "$EXTERNAL_SECRET" force-sync="$(date +%s)" --overwrite
+fi
+
 echo "Deploying to ${KUBERNETES_SERVER}"
 
 echo "Updating ${DEPLOYMENT} in ${NAMESPACE} with ${IMAGE}..."
 
-IFS=',' read -ra containers <<< "$CONTAINER"
+IFS=',' read -ra containers <<<"$CONTAINER"
 
 for container in "${containers[@]}"; do
-    kubectl -n "${NAMESPACE}" set image "deployment/${DEPLOYMENT}" "${container}=${IMAGE}"
+  kubectl -n "${NAMESPACE}" set image "deployment/${DEPLOYMENT}" "${container}=${IMAGE}"
 done
